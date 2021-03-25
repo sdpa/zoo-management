47d2b8325b83cbc7387746908f93a96b489615f3
Joseph Kim <jkim@Josephs-MacBook-Pro-2.local>
2021-03-24 23:45:05 -0500

    app.js routers commented, temporary home.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from "./logo.svg";
 import EnclosureList from "./components/EnclosureList";
+import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Ticket from "./ticket/Ticket"; 
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <EnclosureList></EnclosureList>
+
+
+      {/* <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/ticket" component={Ticket}/>
+        </Switch>
+      </BrowserRouter> */}
     </div>
+
   );
 }
 
