a7198aaf0b0cdb9c24a34f6769cb60495575de2b
gerardoecruz <56847444+gerardoecruz@users.noreply.github.com>
2021-03-31 21:02:35 -0500

    Changed the logic behind a user registering
    Only a customer can register for an account, employees must have pre-made accounts

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -23,20 +23,13 @@ function CreateUser() {
               ></input>
             </div>
             <div className="form-group">
-              <label>User type: </label>
-              <input
-                type="text"
-                required
-                className="form-control"
-              ></input>
-            </div>
-            <div className="form-group">
               <input type="submit" value="Login" className="btn btn-primary" />
             </div>
           </form>
         </div>
       );
-
+      // Only customers should be able to create their own accounts
+      // We have to make employee accounts beforehand, they should end with "@zoo-staff" to signify they're an employee so we know which table to check in db with our queries
 
 }
 
